c20da8ba5997d319e7b217617fbb1b5654eb39d0
Ehsan Fouladi <fooehan3@gmail.com>
2024-03-20 03:37:28 +0330

    create accordion create banner contact us

--- js/style.js
+++ js/style.js
@@ -4,5 +4,16 @@ const close = document.querySelector(".close");
 AOS.init();
 
 menu.addEventListener("click", () => {
-    close.classList.toggle("hidden")
-})
+  close.classList.toggle("hidden");
+});
+
+const items = document.querySelectorAll("#accordion .item .header");
+
+items.forEach((item) => {
+  item.addEventListener("click", (e) => {
+    items.forEach((header) => {
+      header.closest(".item").classList.remove("active");
+    });
+    e.currentTarget.closest(".item").classList.toggle("active");
+  });
+});
